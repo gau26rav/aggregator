d2ac91502e5174f794149c8d35627ccaffc351a5
Gaurav Tiwari <gaurav.tiwari@airtel.com>
2020-02-06 23:44:30 +0530

    Installer prompt added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import Landing from "./component/Landing/Landing";
 import Routes from "./component/Routes/Routes";
+import InstallerPrompt from "./component/InstallerPrompt/InstallerPrompt";
 
 function App() {
   return (
     <div className="App">
+      <InstallerPrompt/>
       <Routes />
     </div>
   );
---
+++ src/component/InstallerPrompt/InstallerPrompt.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+class InstallerPrompt extends React.Component {
+
+    state = { showPrompt: false }
+    componentDidMount() {
+        window.addEventListener('beforeinstallprompt', (event) => {
+            this.deferredPrompt = event;
+            console.log('Before Install event happened')
+        });
+    }
+
+    handleClick = (event) => {
+        this.deferredPrompt.prompt();
+        console.log('deferredPrompt invoked')
+    }
+
+
+    render() {
+        const { showPrompt } = this.state;
+        return (
+            <>
+                {showPrompt && <div id="installer prompt">
+                    <p>Wanna see us at Home page</p>
+                    <button onClick={this.handleClick}>Yes</button>
+                    <button onClick={this.handleClick}>No</button>
+                </div>}
+            </>
+        )
+    }
+}
+
+export default InstallerPrompt
